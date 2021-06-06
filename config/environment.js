4b1c944e76da32c08dcd3a3e3b1611f50ea3b7bf
agkayster <ejike.chiboka@gmail.com>
2021-06-06 17:17:51 +0100

    made adjustments

--- config/environment.js
+++ config/environment.js
@@ -1,7 +1,7 @@
 const port = process.env.PORT || 4000
-const env = process.env.NODE_ENV || 'development'
-const dbURI = process.env.MONGODB_URI || `mongodb://localhost:27017/plots-db-${env}`
+const env = process.env.NODE_ENV || 'development'// we put "env" when we add FRONTEND
+const dbURI = process.env.MONGODB_URI || `mongodb://localhost:27017/plots-db-${env}`// we put "env" when we add FRONTEND
 const secret = process.env.SECRET || 'Tgs5aG_^GH@lKmnN'
 
 
-module.exports = { dbURI, secret, port, env }
+module.exports = { dbURI, secret, port, env }// we put "env" when we add FRONTEND
